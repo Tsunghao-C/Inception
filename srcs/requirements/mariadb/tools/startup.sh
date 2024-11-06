fd21abeba16ed77143f5bfb2994a22dfddf250ae
tsuchen <tsuchen@Inception.tsuchen.virtualbox.org>
2024-11-06 09:28:20 +0100

    Rewrite mariaDB setup in startup script. Avoid using interacting tty that may not work

--- srcs/requirements/mariadb/tools/startup.sh
+++ srcs/requirements/mariadb/tools/startup.sh
@@ -18,51 +18,33 @@ if [ -d "/var/lib/mysql/$MYSQL_DATABASE" ]; then
 	echo "Database already exists..."
 
 else
-	# Secure install for root user and grant privileges
-	mysql_secure_installation << _EOF_
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
-_EOF_
+	echo "Setting up MariaDB..."
 
-	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+	# Directly set up root user and secure the installation
+	mysql -uroot <<EOSQL
+	-- Set root password and disable remote root login
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	DELETE FROM mysql.user WHERE User=''; -- Remove anonymous users
+	DROP DATABASE IF EXISTS test; -- Remove test database
+	FLUSH PRIVILEGES;
 
-	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
+	-- Allow root to connect from any host
+	GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
 
-	# # Set root password and create inital database and user
-	# mysql --user=root <<-EOSQL
-	# 	ALTER USER 'root'@'${DOMAIN_NAME}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-	# 	GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;
-	# 	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-	# 	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-	# 	GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
-	# 	FLUSH PRIVILEGES;
-	# EOSQL
+	-- Create initial database and user with privileges
+	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+	FLUSH PRIVILEGES;
+EOSQL
 
-	# if [ -f /usr/local/bin/wordpress.sql ]; then
-	# 	mysql --user=root --password="${MYSQL_ROOT_PASSWORD}" ${MYSQL_DATABASE} < /usr/local/bin/wordpress.sql
-	# fi
-fi
+	# Import initial data if available
+	if [ -f /usr/local/bin/wordpress.sql ]; then
+	mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" ${MYSQL_DATABASE} < /usr/local/bin/wordpress.sql
+	fi
 
-# Stop MariaDB background process
-
-mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
-
-# Start MariaDB in the foreground
-
-# exec mysqld --bind-address=0.0.0.0
-exec "$@" 
-
-
-
-# # Set root option so that connection wihtout password is not possible
-
-# mysql_secure_installation << _EOF_
-
+# 	# Secure install for root user and grant privileges (not suggested, interactive tty could fail)
+# 	mysql_secure_installation << _EOF_
 # Y
 # $MYSQL_ROOT_PASSWORD
 # $MYSQL_ROOT_PASSWORD
@@ -72,57 +54,16 @@ exec "$@"
 # Y
 # _EOF_
 
-# # Add a root user on 127.0.0.1 to allow remote connection
-# # Flush privileges allow your sql tables to be updated automatically when you modify it
+# 	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-# echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+# 	mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;"
 
-# # Create database and user in the database for wordpress
+fi
 
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+# Stop MariaDB background process
 
-# # Import database in the mysql command line
+mysqladmin -uroot -p"${MYSQL_ROOT_PASSWORD}" shutdown
 
-# mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+# Start MariaDB in the foreground
 
-# fi
-
-# /etc/init.d/mysql stop
-
-# exec "$@"
-
-
-# Okan's version
-# set	-eo pipefail
-
-# # Skip init if it's already there
-
-# if [ ! -d "/var/lib/mysql/is_init" ]; then
-
-# 	mysql_install_db --user=mysql --datadir=/var/lib/mysql
-
-# 	mysqld --user=mysql --skip-networking &
-# 	pid="$!"
-
-# 	# Wait for MySQL to be ready
-# 	until mysqladmin ping -h"localhost" --silent; do
-# 		echo "Waiting for MariaDB to be ready..."
-# 		sleep 2
-# 	done
-
-# 	mysql -e "FLUSH PRIVILEGES;"
-# 	mysql -e "CREATE USER IF NOT EXISTS 'root'@'%';"
-# 	mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '';"
-# 	mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-# 	mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-# 	mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-# 	mysql -e "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-# 	mysql -e "FLUSH PRIVILEGES;"
-
-# 	echo "done" > /var/lib/mysql/is_init
-# 	mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
-# 	wait "$pid"
-# fi
-
-# # Start MySQL server
-# exec gosu mysql "$@"
+exec "$@"
